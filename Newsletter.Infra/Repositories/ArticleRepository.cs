cc01b05e806cfd060128aa64141237cc1f0ae52f
sorza <alexandre.sorza@outlook.com>
2026-07-15 13:23:53 -0300

    feat: implementação fake de repositorios

---
+++ Newsletter.Infra/Repositories/ArticleRepository.cs
@@ -0,0 +1,21 @@
+﻿using Newsletter.Core.Models;
+using Newsletter.Core.Repositories.Abstractions;
+
+namespace Newsletter.Infra.Repositories
+{
+    public class ArticleRepository : IArticleRepository
+    {
+        public async Task<IEnumerable<Article>> GetFromLastWeekAsync(CancellationToken cancellationToken = default)
+        {
+            await Task.Delay(100, cancellationToken);
+
+            return [
+                new Article("Article 1", "https://example.com/article1", "Content of article 1", DateTime.UtcNow.AddDays(-2)),
+                new Article("Article 2", "https://example.com/article2", "Content of article 2", DateTime.UtcNow.AddDays(-5)),
+                new Article("Article 3", "https://example.com/article3", "Content of article 3", DateTime.UtcNow.AddDays(-7)),
+                new Article("Article 4", "https://example.com/article4", "Content of article 4", DateTime.UtcNow.AddDays(-10)),
+                new Article("Article 5", "https://example.com/article5", "Content of article 5", DateTime.UtcNow.AddDays(-12))
+            ];
+        }
+    }
+}
---
+++ Newsletter.Infra/Repositories/SubscriberRepository.cs
@@ -0,0 +1,21 @@
+﻿using Newsletter.Core.Models;
+using Newsletter.Core.Repositories.Abstractions;
+
+namespace Newsletter.Infra.Repositories
+{
+    public class SubscriberRepository : ISubscriberRepository
+    {
+        public async Task<IEnumerable<Subscriber>> GetAllAsync(CancellationToken cancellationToken = default)
+        {
+            await Task.Delay(100, cancellationToken);
+
+            return [
+                new Subscriber("John Doe", "john.doe@exemple.com"),
+                new Subscriber("Jane Doe", "jane.doe@google.com"),
+                new Subscriber("John Smith", "john.smith@opus.com"),
+                new Subscriber("Dotnet Bot", "donet@microsfot.com"),
+                new Subscriber("Software Architect", "arch@tect.com")
+            ];
+        }
+    }
+}
