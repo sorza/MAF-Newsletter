981417d91c0d0da461ac7734b1aeebfa4c36f0ae
sorza <alexandre.sorza@outlook.com>
2026-07-15 13:32:26 -0300

    feat: Injetando dependencias

---
+++ Newsletter.Infra/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Newsletter.Core.Repositories.Abstractions;
+using Newsletter.Core.Services.Abstractions;
+using Newsletter.Infra.Repositories;
+using Newsletter.Infra.Services;
+
+namespace Newsletter.Infra
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services) {
+
+            // services.AddScoped<INewsletterService, NewsletterService>();
+            services.AddScoped<IEmailService, EmailService>();
+           
+            return services;
+        }
+
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IArticleRepository, ArticleRepository>();
+            services.AddScoped<ISubscriberRepository, SubscriberRepository>();
+
+            return services;
+        }
+    }
+}
---
+++ Newsletter.Infra/Services/NewsletterService.cs
@@ -0,0 +1,12 @@
+﻿using Newsletter.Core.Services.Abstractions;
+
+namespace Newsletter.Infra.Services
+{
+    internal class NewsletterService : INewsletterService
+    {
+        public Task SendAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
