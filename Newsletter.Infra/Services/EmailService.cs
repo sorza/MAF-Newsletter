d9158c81b45bb0a0b5613ba863db7ff551827823
sorza <alexandre.sorza@outlook.com>
2026-07-15 13:27:15 -0300

    feat: Implementação fake Email Service

---
+++ Newsletter.Infra/Services/EmailService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+using Newsletter.Core.Services.Abstractions;
+
+namespace Newsletter.Infra.Services
+{
+    public class EmailService(ILogger<EmailService> logger) : IEmailService
+    {     
+        public async Task SendAsync(string toName, string toEmail, string subject, string body, CancellationToken cancellationToken = default)
+        {
+           await Task.Delay(150, cancellationToken);
+            logger.LogInformation("Enviando email para {ToName} ({ToEmail}) com assunto '{Subject}'", toName, toEmail, subject);
+        }
+    }
+}
